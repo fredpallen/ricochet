3f879a2dfd0725509e012a4176e6672d8a5c1b55
fredpallen <fred.p.allen@gmail.com>
2017-06-19 17:51:04 -0700

    Fix backward search bug: no backing wall
    We were allowing the backward search to happen in a direction that
    didn't have a backing wall. This made it seem that we could stop
    anywhere without hitting a wall and led to spurious solutions.
    
    Now a backward search is only started if it is in the direction opposite
    to a wall.

--- src/ricochet.c
+++ src/ricochet.c
@@ -1,5 +1,6 @@
 #include "ricochet.h"
 
+#include <assert.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -26,6 +27,11 @@ typedef struct {
 
 static Direction rot90(Direction d) {
     Direction result = {.x = -d.y, .y = d.x}; 
+    return result;
+}
+
+static Direction rot180(Direction d) {
+    Direction result = {.x = -d.x, .y = -d.y};
     return result;
 }
 
@@ -84,34 +90,43 @@ static void search_forward(
     }
 }
 
+// Assumes is_wall(walls, start, rot180(dir)) so that we're searching
+// backward from a place we could actually stop.
 static void search_backward(
         CellStates *states,
         const Walls *walls,
         Position start,
+        Direction dir,
         int level) {
+    assert(is_wall(walls, start, rot180(dir)));
     if (!level) {
         return;
     }
     const Route route = states->states[start.x][start.y].route;
     const int length = route.length;
-    for (int i = 0; i < 4; ++i) {
-        Position cursor = start;
-        Direction d = directions[i];
-        while (!is_wall(walls, cursor, d)) {
-            cursor.x += d.x;
-            cursor.y += d.y;
-            CellState *state = &states->states[cursor.x][cursor.y];
-            int is_new = !state->seen || state->route.length > length + 1;
-            int has_orthog =
-                is_wall(walls, cursor, rot90(d)) ||
-                is_wall(walls, cursor, rot270(d));
-            if (is_new && has_orthog) {
-                state->seen = true;
-                state->route = route;
-                state->route.moves[route.length] = d;
-                ++state->route.length;
-                search_backward(states, walls, cursor, level - 1);
-            }
+    Position cursor = start;
+    while (!is_wall(walls, cursor, dir)) {
+        cursor.x += dir.x;
+        cursor.y += dir.y;
+        CellState *state = &states->states[cursor.x][cursor.y];
+        // Don't continue states that we have reached with fewer moves.
+        if (state->seen && state->route.length <= length) {
+            continue;
+        }
+        Direction d90 = rot90(dir);
+        Direction d270 = rot270(dir);
+        bool is_wall_d90 = is_wall(walls, cursor, d90);
+        bool is_wall_d270 = is_wall(walls, cursor, d270);
+        if (is_wall_d90 || is_wall_d270) {
+            state->seen = true;
+            state->route = route;
+            state->route.moves[route.length] = dir;
+            ++state->route.length;
+        }
+        if (is_wall_d90) {
+            search_backward(states, walls, cursor, d270, level - 1);
+        } else if (is_wall_d270) {
+            search_backward(states, walls, cursor, d90, level - 1);
         }
     }
 }
@@ -134,7 +149,16 @@ Route find_route(
     backward.states[end.x][end.y].route.length = 0;
 
     search_forward(&forward, walls, start, RICOCHET_MAX_MOVES);
-    search_backward(&backward, walls, end, RICOCHET_MAX_MOVES);
+
+    // Only search backward along a direction if there is a wall in the
+    // other direction.
+    for (int i = 0; i < 4; ++i) {
+        Direction d = directions[i];
+        if (is_wall(walls, end, d)) {
+            search_backward(
+                    &backward, walls, end, rot180(d), RICOCHET_MAX_MOVES);
+        }
+    }
 
     route.length = 2 * RICOCHET_BOARD_WIDTH + 2;
     for (int x = 0; x < RICOCHET_BOARD_WIDTH; ++x) {
