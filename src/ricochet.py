f61abc5d53874eedb4d0d8c0f5310fdd944d95b3
fredpallen <fred.p.allen@gmail.com>
2017-06-23 22:58:31 -0700

    Support string representation of walls

--- src/ricochet.py
+++ src/ricochet.py
@@ -32,49 +32,100 @@ class Route(ctypes.Structure):
             ('moves', MAX_MOVES * Direction),
             ]
 
+def StrToWalls(s):
+    """Makes a Walls instance from a string representation.
+
+    The string representation uses '|' for vertical wall, '=' for horizontal
+    wall, + for cell corner, and ' ' for no wall.
+
+    4x4 example
+    '''
+    +=+=+=+=+
+    |   |   |
+    + +=+=+ +
+    |     | |
+    + +=+ + +
+    |       |
+    + + + + +
+    | |     |
+    +=+=+=+=+
+    '''
+    """
+    lines = s.strip().splitlines()
+    row_lines = [line.strip() for line in lines[0::2]]
+    col_lines = [line.strip() for line in lines[1::2]]
+    if len(row_lines) != BOARD_WIDTH + 1:
+        raise Exception('Saw %s rows' % len(row_lines))
+    if len(col_lines) != BOARD_WIDTH:
+        raise Exception('Saw %s cols' % len(col_lines))
+    row_chars = [line[1::2] for line in row_lines]
+    col_chars = [line[0::2] for line in col_lines]
+    for i, chars in enumerate(row_chars):
+        if len(chars) != BOARD_WIDTH:
+            raise Exception('Saw %s chars for rows %s' % (len(chars), i))
+        if not set(chars).issubset({' ', '='}):
+            raise Exception('Illegal character in rows %s' % i)
+    for i, chars in enumerate(col_chars):
+        if len(chars) != BOARD_WIDTH + 1:
+            raise Exception('Saw %s chars for cols %s' % (len(chars), i))
+        if not set(chars).issubset({' ', '|'}):
+            raise Exception('Illegal character in cols %s' % i)
+    rows = WallsRows(
+            *[
+                WallsRow(*[1 if c == '=' else 0 for c in chars])
+                for chars in row_chars
+            ])
+    cols = WallsCols(
+            *[
+                WallsCol(*[1 if c == '|' else 0 for c in chars])
+                for chars in col_chars
+            ])
+    return Walls(rows=rows, cols=cols)
+
+
 if __name__ == '__main__':
     libricochet.find_route.restype = Route
-    
-    walls = Walls(
-            rows=WallsRows(
-                WallsRow(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-                WallsRow(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
-                ),
-            cols=WallsCols(
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                WallsCol(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-                ))
+
+    walls = StrToWalls(
+            """
+            +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + +=+=+ + + + + + + +
+            |             |   |             |
+            + + + + + + + + + + + + + + + + +
+            |             |   |             |
+            + + + + + + + +=+=+ + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |               |               |
+            + + + + + + + + + + + + + + + +=+
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |                               |
+            + + + + + + + + + + + + + + + + +
+            |               |               |
+            +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+            """)
     start = Position(x=0, y=0)
     end = Position(x=15, y=15)
 
     route = libricochet.find_route(ctypes.byref(walls), start, end)
     print('route.length =', route.length)
+    for i in range(route.length):
+        print('x =', route.moves[i].y, ', y = ', route.moves[i].x)
